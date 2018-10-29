d2aa5efa658fe91596c3013ea674233c43f651ad
Alan Harnum <aharnum@ocadu.ca>
2018-10-29 16:37:56 -0400

    Datestamp line entries in files in style of new logger.

--- botbot_parser.py
+++ botbot_parser.py
@@ -12,6 +12,11 @@ ex: python botbot_parser.py fluid-design.log fluid-design 2018-09-20
 import datetime
 import sys
 import os
+import re
+
+def get_formatted_log_line(log_line, current_date):
+    fixed_ts = re.sub(r'\[([0-9][0-9]:[0-9][0-9]:[0-9][0-9])\]', current_date.strftime("%Y-%m-%d") + r'T\1', log_line)
+    return fixed_ts
 
 log_file_name = sys.argv[1]
 
@@ -32,12 +37,15 @@ log_file = open(log_file_name, "r")
 # print the first line to file based on the entered start_date
 
 f = open(channel_name + os.sep + channel_name + "-" + start_date + ".log", "w")
-f.write(log_file.readline())
+first_entry = log_file.readline()
+for log_line in first_entry.split("\\n"):
+    f.write(get_formatted_log_line(log_line, datetime.datetime.strptime(start_date.strip(), "%Y-%m-%d")) + "\n")
 f.close()
 
 current_date = None
 current_file = None
 
+# process the remaining lines
 for line in log_file:
     try:
         # line is a date, update the file we're using
@@ -45,4 +53,8 @@ for line in log_file:
         current_file = open(channel_name + os.sep + channel_name + "-" + line.strip() + ".log", "w")
     except ValueError:
         # not a date, write it to the file
-        current_file.write(line.strip().replace("\\n", "\n"))
+        # split on the newline
+        log_for_day = line.split("\\n")
+        for log_line in log_for_day:
+            fixed_ts = get_formatted_log_line(log_line, current_date)
+            current_file.write(fixed_ts + "\n")
